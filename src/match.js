57b3f4aaa7a0035d7671f819e5803e0297f97adc
Cem Sönmez <cem.sonmez@outlook.com.tr>
2026-01-04 23:23:55 +0000

    Import dayjs package to Match class

--- src/match.js
+++ src/match.js
@@ -1,9 +1,11 @@
+const dayjs = require('dayjs')
+
 class Match {
   constructor(homeTeam, awayTeam, stadium, date) {
     this.homeTeam = homeTeam;
     this.awayTeam = awayTeam;
     this.stadium = stadium;
-    this.date = date;
+    this.date = dayjs(date).format('DD/MM/YYYY HH:mm');
   }
 }
 
